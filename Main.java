081eba31b164f607be62c4dee82d3afe1d960479
S.Mukesh <mukeshhema24@gmail.com>
2025-08-29 11:24:47 +0530

    Array_list

--- Main.java
+++ Main.java
@@ -1,29 +1,44 @@
-package aggregation;
+package array_list;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
-	public static void main(String args[]) {
-		// aggregation = represents a "has-a" relationship between objects.
-		//                One object contains another object as part of its structure.
-		//				  but those object can exist independently
+	
+	// ArrayList = A resizeable array that stores objects (autoboxing)
+	//             Array are fixed in size,but ArrayLists can change.
+	// you can't use primitive
+	public static void main(String[] args) {//  | we don't want to mention datatype
+		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Double > list1 = new ArrayList<>();
+		ArrayList<String> fruits = new ArrayList<>();
+
+		list.add(3);
+		list.add(2);
+		list.add(1);
+		list1.add(3.14);
+		list1.add(54.12);
+		fruits.add("Apple");
+		fruits.add("orange");
+		fruits.add("banana");
+		fruits.add("guva");
 		
-		Book book1 = new Book("harry potter 1",423);
-		Book book2 = new Book("shiva",423);
-		Book book3 = new Book("harry potter 2",423);
+/*	fruits.remove(0);
+		fruits.set(0, "Pineapple");
 		
-		Book[] books = {book1,book2,book3}; // store object
+		System.out.println(list); //element inside the list
+		System.out.println(list1);
+		System.out.println(fruits);
+		System.out.println(fruits.get(0));
+		System.out.println(fruits.size());
+		*/
+		Collections.sort(fruits); // to sort acending
+		System.out.println(fruits);
 		
-		/* for(Book book:books) { // access each object data's
-			System.out.println(book.displayInfo());
-		} */
-		
-		Library library = new Library("NYC Public Library", 1897, books);
-		
-		library.displayInfo();
-		/*
-		 *  library:
-		 *  	displayInfo:
-		 *  		book:
-		 *  			displayInfo
-		 * */
+		for(String fruit:fruits) { // print fruits value line by line using enhanced for loop
+			System.out.println(fruit);
+		}
 	}
+ 
 }
