d3d71bf772338f29c004889b7dc82dbc1da3c6e1
S.Mukesh <mukeshhema24@gmail.com>
2025-08-29 11:29:25 +0530

    exception

--- Main.java
+++ Main.java
@@ -1,28 +1,51 @@
-package dynamic_polymorphism;
+package exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// dynamic / runtime polymorphism
-		// when the method that gets executed is decided
-		// at runtime based on the actual type of the object
+		// Exception - an event that interrupts the normal flow of a program
+		// 			   (Dividing by zero, file not found, mismatch input types)
+		//			   Surround any dangeroius code with a try{} block
+		//			   try{},catch{},finally{}
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		Animal animal;
-		
-		System.out.println("Would you like dog or cat? dog-1,cat-2");
-		int choice = scanner.nextInt();
-		
-		if(choice == 1) {
-			animal = new Dog();
-			animal.speak();
+		Scanner scan = new Scanner(System.in);
+		/*
+		try {
+			System.out.println(1/0); // dangerous code
 		}
-		else {
-			animal = new Cat();
-			animal.speak();
+		catch(ArithmeticException e) {
+		 //   error_name          name
+			System.out.println("You can't divide by zero!");
+		}
+		*/
+		/*
+		try {
+			System.out.print("Enter a number: ");
+			int number = scan.nextInt();
+			System.out.println(number);
+		}
+		catch(InputMismatchException e) {
+			System.out.println("kindly enter a number");
+		} */
+		
+		try {
+			System.out.print("enter your salary: ");
+			int salary = scan.nextInt();
+			scan.next();
+		}
+		catch (ArithmeticException e) {
+			System.out.println("arithmetric error");
+		}
+		catch (Exception e) {
+			System.out.println("error bro find it!");
+		}
+		finally {  // optional
+			scan.close();
+			// we can close the file
+			System.out.println("this always execute");
 		}
 	}
 
